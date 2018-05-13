9af58d292ce21505a905921c97c5f7068e3d9689
Joanna Buchała <joanna.b2@op.pl>
2018-05-13 20:22:33 +0200

    add counter and lifecycle

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 var Counter = React.createClass({
+	getDefaultProps() {
+		console.log('Here I could set some default props for my component, which I can override later.');
+	},
+
     getInitialState: function() {
         return {
             counter: 0
         };
+    },
+
+    componentWillMount() {
+    	console.log('Here I could set some initial state or global events like window or document.');
+    },
+
+    componentDidMount() {
+    	console.log('Here i could use Ajax call.');
     },
 
     increment: function() {
@@ -16,19 +28,39 @@ var Counter = React.createClass({
         });
     },
 
-  render: function() {
+     componentWillReceiveProps(nextProps) {
+     	console.log('I can check if some props are changed, and set new state.');
+     },
+
+     shouldComponentUpdate(nextProps, nextState) {
+     	console.log('Here I can decide to re-render or not. If i have big project then I would return false and prevent slowing down my page.');
+     	return true;
+     },
+
+     componentWillUpdate(nextProps, nextState) {
+     	console.log('Here I could set new variables based on the props.');
+     },
+
+     componentDidUpdate(prevProps, prevState) {
+     	console.log('Updating the DOM with new values.');
+     },
+
+     componentWillUnmount() {
+     	console.log('Here I could remove some events for example or just cancel my component.');
+     },
+
+	 render: function() {
         return  React.createElement('div', {}, 
                     React.createElement('button', {onClick: this.increment},
                         React.createElement('span', {}, '+')),
-                        React.createElement('p', {}, 'licznik: ' + this.state.counter),
+                        React.createElement('p', {}, 'Counter: ' + this.state.counter),
                         React.createElement('button', {onClick: this.decrement},
                         React.createElement('span', {}, '-'))
 
                 )
+   		}
 
 
-   		 }
 });
-
 var element = React.createElement(Counter);
 ReactDOM.render(element, document.getElementById('app'));
